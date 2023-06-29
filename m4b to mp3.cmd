3ab3b6d8008452a1c8d8ba4787d8a413757a68ed
flameSla <39356103+flameSla@users.noreply.github.com>
2023-06-29 18:54:09 +0700

    Update m4b to mp3.cmd

--- m4b to mp3.cmd
+++ m4b to mp3.cmd
@@ -1,7 +1,6 @@
-@set x=1
 FOR %%a IN (*.m4b) DO (
-    "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "%%a" -acodec libmp3lame -ar 22050 -vn "%x%.mp3"
-    py m4b_to_mp3.py -f "%x%.mp3"
-    del "%x%.mp3"
-    @set /a x+=1
+    "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "%%a" -acodec libmp3lame -ar 22050 -vn "%%~na.mp3"
+    py m4b_to_mp3.py -f "%%~na.mp3"
+    del "%%~na.mp3"
 )
+pause
