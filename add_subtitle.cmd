411e1f2c9a76334ebbdf318666d9a2f9fecbfc75
flameSla <39356103+flameSla@users.noreply.github.com>
2023-09-24 22:57:52 +0700

    Update add_subtitle.cmd

--- add_subtitle.cmd
+++ add_subtitle.cmd
@@ -1,5 +1,6 @@
 rem FOR %%a IN (*.mkv) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "%%~na.mkv" -sub_charenc UTF-8 -i "%%~na.ass" -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=rus -metadata:s:s:0 handler="Russian" "new_%%~na.mkv"
-FOR %%a IN (*.mkv) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "%%~na.mkv" -i "%%~na.ass" -c:v copy -c:a copy -c:s srt "new_%%~na.mkv"
+FOR %%a IN (*.mkv) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "%%~na.mkv" -i "%%~na.ass" -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=rus -metadata:s:s:0 handler="Russian" "new_%%~na.mkv"
+FOR %%a IN (*.mp4) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "%%~na.mp4" -i "%%~na.ass" -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=rus -metadata:s:s:0 handler="Russian" "new_%%~na.mkv"
 
 rem To make the second subtitle stream the default stream and remove the default disposition from the first subtitle stream:
 rem ffmpeg -i in.mkv -c copy -disposition:s:0 0 -disposition:s:1 default out.mkv
