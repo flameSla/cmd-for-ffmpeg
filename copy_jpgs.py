f7607ee740458ae198afc6054d9c3ee52ced8341
flameSla <39356103+flameSla@users.noreply.github.com>
2024-01-02 22:04:49 +0700

    Update copy_jpgs.py

--- copy_jpgs.py
+++ copy_jpgs.py
@@ -1,38 +1,30 @@
-import inspect
-import os
-import sys
 from os import walk
 import shutil
+from pathlib import Path
+
 
 ####################################
-def get_script_dir(follow_symlinks=True):
-    if getattr(sys, "frozen", False):  # py2exe, PyInstaller, cx_Freeze
-        path = os.path.abspath(sys.executable)
-    else:
-        path = inspect.getabsfile(get_script_dir)
-    if follow_symlinks:
-        path = os.path.realpath(path)
-    return os.path.dirname(path)
+def get_current_working_directory():
+    return Path().resolve()
 
 
 ######################################
 #
 # main
 if __name__ == "__main__":
-
-    mypath = get_script_dir()
+    mypath = get_current_working_directory()
+    print(mypath)
+    print()
     dirpaths = {}
-    for (dirpath, dirnames, filenames) in walk(mypath):
-        dirpath = dirpath.replace(mypath, "")
+    for dirpath, dirnames, filenames in walk(mypath):
+        dirpath = dirpath.replace(str(mypath), "")
         if dirpath:
             for filename in filenames:
                 if dirpath not in dirpaths:
                     dirpaths[dirpath] = len(dirpaths)
                 old_filename = "{}\\{}".format(dirpath[1:], filename)
-                print(old_filename, end="\t")
                 new_filename = "{0:03d} - {1:}".format(dirpaths[dirpath], filename)
-                print(new_filename)
                 shutil.copy(old_filename, new_filename)
-                print("{} - {}".format(old_filename, new_filename))
+                print("{} -> {}".format(old_filename, new_filename))
 
     # print("dirpaths = ", type(dirpaths), dirpaths)
