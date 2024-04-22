ad70545c34e12b46c45075a919f73df1bcddcb35
flameSla <39356103+flameSla@users.noreply.github.com>
2024-04-23 04:24:59 +0700

    Update video.cmd 04:24 23/04/2024

--- video.cmd
+++ video.cmd
@@ -31,12 +31,14 @@ echo 1 - hevc_nvenc
 echo 2 - hevc_qsv
 echo 3 - av-1
 echo 4 - hevc_nvenc -b:v 1200k
+echo 5 - hevc_nvenc -b:v 1200k ac3 192k
 echo.
 choice /c 1234 /m "default - 1" /t 10 /d 1
 if "%ERRORLEVEL%"=="1" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a -map 0:v -map 0:a %resize%-c:v hevc_nvenc -preset slow -qp 26 -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv" )
 if "%ERRORLEVEL%"=="2" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_qsv -preset slow -global_quality 26 -c:a copy -c:s copy "%%~dpahevc_Intel_%%~na.mkv" )
 if "%ERRORLEVEL%"=="3" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg-2023-08-14\bin\ffmpeg.exe" -i %%a %resize%-c:v libaom-av1 -crf 30 -cpu-used 8 -row-mt 1 -tiles 4x1 -c:a copy -c:s copy "%%~dpahevc_av1_%%~na.mkv" )
 if "%ERRORLEVEL%"=="4" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v 1200k -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv" )
+if "%ERRORLEVEL%"=="5" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v 1200k -c:a ac3 -b:a 192k -c:s copy "%%~dpahevc_nvenc_%%~na.mkv" )
 
 del files.tmp
 
