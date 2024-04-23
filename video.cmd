64bfdc0f95021a76713fd5f7197bcdd018307182
flameSla <39356103+flameSla@users.noreply.github.com>
2024-04-23 22:37:17 +0700

    Update video.cmd 22:37 23/04/2024

--- video.cmd
+++ video.cmd
@@ -38,7 +38,7 @@ if "%ERRORLEVEL%"=="1" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Pr
 if "%ERRORLEVEL%"=="2" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_qsv -preset slow -global_quality 26 -c:a copy -c:s copy "%%~dpahevc_Intel_%%~na.mkv" )
 if "%ERRORLEVEL%"=="3" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg-2023-08-14\bin\ffmpeg.exe" -i %%a %resize%-c:v libaom-av1 -crf 30 -cpu-used 8 -row-mt 1 -tiles 4x1 -c:a copy -c:s copy "%%~dpahevc_av1_%%~na.mkv" )
 if "%ERRORLEVEL%"=="4" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v 1200k -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv" )
-if "%ERRORLEVEL%"=="5" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v 1200k -c:a ac3 -b:a 192k -c:s copy "%%~dpahevc_nvenc_%%~na.mkv" )
+if "%ERRORLEVEL%"=="5" ( FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v 1200k -c:a ac3 -ac 2 -b:a 192k -c:s copy "%%~dpahevc_nvenc_%%~na.mkv" )
 
 del files.tmp
 
