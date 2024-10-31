8dc629e698b85f6527866b706842e3f4982cf843
flameSla <39356103+flameSla@users.noreply.github.com>
2024-11-01 03:27:27 +0700

    Create cut_mkv.cmd

---
+++ cut_mkv.cmd
@@ -0,0 +1,7 @@
+"C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i "Factorio Space Age Speedrun Default Settings.mkv" -c:v copy -c:a copy  -f segment -segment_time 3600 -y Factorio_%%003d.mkv
+
+
+
+
+
+pause
