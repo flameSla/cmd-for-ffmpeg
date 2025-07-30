b2e8e4fff5ea557974f92acd54c9266c77a9ad6e
flameSla <39356103+flameSla@users.noreply.github.com>
2025-07-30 22:13:09 +0700

    Update video.cmd

--- video.cmd
+++ video.cmd
@@ -13,6 +13,8 @@ FOR /r %%a IN (*.wmv) DO echo "%%a" >> files.tmp
 
 notepad.exe files.tmp
 
+set ffmpeg="C:\Program Files\ffmpeg\bin\ffmpeg.exe"
+
 @rem resize
 cls
 echo.
@@ -21,8 +23,8 @@ echo 2 - resize ( 1920 )
 echo 3 - resize ( 1280 )
 echo 4 - resize ( 640 )
 echo.
-choice /c 1234 /m "default - 1" /t 30 /d 1
 set resize=
+choice /c 1234 /m "default - 1" /t 30 /d 1
 if "%ERRORLEVEL%"=="2" ( set resize=-vf scale=1920:-1 )
 if "%ERRORLEVEL%"=="3" ( set resize=-vf scale=1280:-1 )
 if "%ERRORLEVEL%"=="4" ( set resize=-vf scale=640:-1 )
@@ -36,21 +38,25 @@ choice /c 123 /m "default - 1" /t 30 /d 1
 
 goto answer%ERRORLEVEL%
 :answer1
-FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -qp 26 -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv"
+FOR /F "usebackq delims==" %%a IN (files.tmp) DO %ffmpeg% -i %%a %resize%-c:v hevc_nvenc -preset slow -qp 26 -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv"
 goto exit
 
 :answer2
 set /p vbitrate=Enter the video bitrate (1200k):
-FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v %vbitrate% -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv"
+FOR /F "usebackq delims==" %%a IN (files.tmp) DO %ffmpeg% -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v %vbitrate% -c:a copy -c:s copy "%%~dpahevc_nvenc_%%~na.mkv"
 goto exit
 
 :answer3
 set /p vbitrate=Enter the video bitrate (1200k):
 set /p abitrate=Enter the audio bitrate (192k):
-FOR /F "usebackq delims==" %%a IN (files.tmp) DO "C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v %vbitrate% -c:a ac3 -ac 2 -b:a %abitrate% -c:s copy "%%~dpahevc_nvenc_%%~na.mkv"
+FOR /F "usebackq delims==" %%a IN (files.tmp) DO %ffmpeg% -i %%a %resize%-c:v hevc_nvenc -preset slow -b:v %vbitrate% -c:a ac3 -ac 2 -b:a %abitrate% -c:s copy "%%~dpahevc_nvenc_%%~na.mkv"
 goto exit
 
 :exit
+
+echo resize=%resize% > out.txt
+echo vbitrate=%vbitrate% >> out.txt
+echo abitrate=%abitrate% >> out.txt
 
 del files.tmp
 
